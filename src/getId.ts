2ccd5e76496da10f03731b6ae568b411a6bff6d3
David Severwright <code@davidsev.co.uk>
2024-03-07 19:56:21 +0000

    fixed the extension ID

--- src/getId.ts
+++ src/getId.ts
@@ -1,4 +1,4 @@
-export const baseId = 'uk.co.davidsev.owlbear-tokenfreeze';
+export const baseId = 'uk.co.davidsev.owlbear-ruler';
 
 export default function getId (id ?: string): string {
     if (id)
