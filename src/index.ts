2de07faf49cbbaa9a7a919158a50467662d3dcc1
David Severwright <code@davidsev.co.uk>
2024-04-01 07:57:32 +0100

    wait for the grid info to load before creating the tool

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
 import OBR from '@owlbear-rodeo/sdk';
 import { Tool } from './Tool';
+import { grid } from '@davidsev/owlbear-utils';
 
 window.init = function () {
     OBR.onReady(async () => {
+        await grid.init();
         OBR.tool.createMode(new Tool());
     });
 };
